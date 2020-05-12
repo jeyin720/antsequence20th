3ee602a37773bf82bb418b0a80c3373f449facf7
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-12 13:23:44 +0900

    초기 커밋.

---
+++ antsequence20th/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antsequence20th
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;
+            String n = "11";
+            String num=n;
+            int cnt = 1;
+            Console.WriteLine(cnt + "번째 수열: 1");
+            while (cnt< 20)
+            {
+                cnt++;
+                Console.WriteLine(cnt+"번째 수열: "+num);
+                
+                k = 0;
+                
+                n = num;
+                char n1 = n[0];
+                num = "";
+                for (int i = 0; i < n.Length; i++)
+                {
+                    
+                    if (n1 == n[i])
+                    {
+                        k++;
+                    }
+                    else
+                    {
+                        num += n1 + "" + k;
+                        k = 1;
+                        n1 = n[i];
+                    }
+                    
+                }
+                num += n1 + "" + k;
+            }
+            
+        }
+    }
+}
